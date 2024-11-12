b5dcc4d0f1f22c2e8d4aa43b33cb97b4189586b7
Sergej Garkusha <Tenebrann@googlemail.com>
2024-11-12 14:07:23 +0100

    update storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
-import path from 'path';
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const config: StorybookConfig = {
-  stories: [path.join(__dirname, '../src/**/*.mdx'), path.join(__dirname, '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)')],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-onboarding',
@@ -16,7 +13,7 @@ const config: StorybookConfig = {
     name: '@storybook/react-webpack5',
     options: {},
   },
-  staticDirs: [path.join(__dirname, '../public')],
+  staticDirs: ['../public'],
 };
 
 export default config;
