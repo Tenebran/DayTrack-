379fac1199bb136235f6ad32987f77ba414fb18d
Sergej Garkusha <Tenebrann@googlemail.com>
2024-11-13 12:11:47 +0100

    chore(storybook): disable ESLintWebpackPlugin to prevent build blocking
    - Removed ESLintWebpackPlugin in Storybook Webpack configuration to allow Storybook to build even with ESLint errors.
    - Updated Storybook configuration in main.ts to filter out ESLintWebpackPlugin from plugins.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import { Configuration } from 'webpack';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,6 +15,13 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ['../public'],
+  webpackFinal: async (config: Configuration) => {
+    config.plugins = config.plugins?.filter(
+      (plugin) => plugin?.constructor.name !== 'ESLintWebpackPlugin'
+    );
+
+    return config;
+  },
 };
 
 export default config;
