76de66f3ea604481e626e0d8cae43e2594dbf917
Sergej Garkusha <Tenebrann@googlemail.com>
2024-11-13 13:30:47 +0100

    fix  integration/setupTests

--- integration/setupTests.js
+++ integration/setupTests.js
@@ -1,16 +1,16 @@
-import { existsSync, mkdirSync, writeFileSync } from 'fs';
-import { basename, resolve, join } from 'path';
-import { toMatchImageSnapshot } from 'jest-image-snapshot';
+const fs = require('fs');
+const path = require('path');
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
 expect.extend({
   toMatchImageSnapshot(received, ...args) {
     const testName = this.currentTestName.replace(/\s+/g, '-').replace(/[^a-z0-9-]/gi, '').toLowerCase();
-    const testFileName = basename(this.testPath, '.js').replace(/\./g, '-');
+    const testFileName = path.basename(this.testPath, '.js').replace(/\./g, '-');
 
     const snapshotIdentifier = `${testFileName}-${testName}-1-snap`; 
 
-    const customSnapshotsDir = resolve(__dirname, '__image_snapshots__');
-    const customDiffDir = resolve(customSnapshotsDir, '__diff_output__');
+    const customSnapshotsDir = path.resolve(__dirname, '__image_snapshots__');
+    const customDiffDir = path.resolve(customSnapshotsDir, '__diff_output__');
 
     const result = toMatchImageSnapshot.call(this, received, {
       customSnapshotsDir,
@@ -24,14 +24,14 @@ expect.extend({
     if (!result.pass) {
       console.log('Snapshot does not match. Creating a new screenshot for comparison.');
 
-      const newSnapshotDir = resolve(customSnapshotsDir, '__corrected_snapshots__');
-      const newSnapshotPath = join(newSnapshotDir, `${snapshotIdentifier}.png`);
+      const newSnapshotDir = path.resolve(customSnapshotsDir, '__corrected_snapshots__');
+      const newSnapshotPath = path.join(newSnapshotDir, `${snapshotIdentifier}.png`);
 
-      if (!existsSync(newSnapshotDir)) {
-        mkdirSync(newSnapshotDir, { recursive: true });
+      if (!fs.existsSync(newSnapshotDir)) {
+        fs.mkdirSync(newSnapshotDir, { recursive: true });
       }
 
-      writeFileSync(newSnapshotPath, received);
+      fs.writeFileSync(newSnapshotPath, received);
       console.log('New screenshot successfully saved at path:', newSnapshotPath);
     }
 
