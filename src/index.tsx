6169e1841911363f7ec5a3afd857de4f8df9a171
Sergej Garkusha <Tenebrann@googlemail.com>
2024-11-18 10:59:03 +0100

    Install react-router-dom v6 and formik, add Login component

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { App } from './app/App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 export const darkTheme = createTheme({
   palette: {
@@ -14,10 +15,12 @@ export const darkTheme = createTheme({
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <Provider store={store}>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </BrowserRouter>
 );
